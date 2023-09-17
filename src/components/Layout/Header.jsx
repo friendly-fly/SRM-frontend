1d55d8910ed4e2580fdb7dacb52713b280d25296
manish7479dlp <manish7479dlp@gmail.com>
2023-09-17 12:26:23 +0530

    ui: make header responsive

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div className="row align-items-center px-5" style={{ height: "8vh", background: "#C4E1F6" }}>
+        <div className="d-flex justify-content-space-between align-items-center  px-3 px-sm-5" style={{ height: "8vh", background: "#C4E1F6" }}>
 
-            <div className="col h2 project-title">
+            <div className="flex-fill h2 project-title ">
                 SRM
             </div>
-            <div className="col text-end ">
+            <div className="flex-fill text-end ">
                 <Link to="/login" className='btn button-style py-1' >Login</Link>
             </div>
 
