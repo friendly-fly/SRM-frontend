4ddf2e3c40a550b49e1963097e884309329048bb
manish7479dlp <manish7479dlp@gmail.com>
2023-09-20 19:03:06 +0530

    ui: make header icon as a link

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from "../../assets/logo.png"
 
 const Header = () => {
     return (
         <div className="d-flex justify-content-space-between align-items-center  px-3 px-sm-5" style={{ height: "8vh", background: "#C4E1F6" }}>
 
-            <div className="flex-fill h2 project-title ">
-                SRM
+            <div className=" " >
+                {/* <div className=" bg-primary " style={{ width: "140px" }}> */}
+                <Link to="/" >
+                    <img className='img-fluid' src={logo} alt='logo' style={{ cursor: 'pointer', height: "120px" }} />
+
+                </Link>
+
+                {/* </div> */}
+
             </div>
             <div className="flex-fill text-end ">
                 <Link to="/login" className='btn button-style py-1' >Login</Link>
+
+
             </div>
 
         </div>
