ec98c4d1dfed4f80253ce65e56d0e44fa165bab4
manish7479dlp <manish7479dlp@gmail.com>
2023-09-22 22:00:37 +0530

    ui: modifie ui style in mobile view

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -5,24 +5,29 @@ import LinkButton from '../LinkButton'
 
 const Header = () => {
     return (
-        <div className="d-flex justify-content-space-between align-items-center  px-3 px-sm-5" style={{ height: "8vh", background: "#C4E1F6" }}>
+        <>
+            <div className="d-flex  justify-content-space-between align-items-center  px-3 px-sm-5 customHeaderStyle" >
 
-            <div className=" " >
-                {/* <div className=" bg-primary " style={{ width: "140px" }}> */}
-                <Link to="/" >
+                <Link to="/">
                     <img className='img-fluid' src={logo} alt='logo' style={{ cursor: 'pointer', height: "120px" }} />
 
                 </Link>
 
-                {/* </div> */}
+
+                <div className="flex-fill text-end ">
+                    <LinkButton btnText={"Login"} link="/login" />
+
+                </div>
+
 
             </div>
-            <div className="flex-fill text-end ">
-                <LinkButton btnText={"Login"} link="/login" />
+        </>
 
-            </div>
 
-        </div>
+
+
+
+
     )
 }
 
