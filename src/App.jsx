0c8aec1d5eaf3370301e76654d69d5159f323c7a
manish7479dlp <manish7479dlp@gmail.com>
2024-03-04 00:26:48 +0530

    refac: remove footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AdmissionForm from "./pages/student/AdmissionForm";
 import StudentDetails from "./pages/student/StudentDetails";
+import StudentsList from "./pages/student/StudentsList";
 
 const App = () => {
   return (
@@ -14,11 +15,14 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admission-form" element={<AdmissionForm/>} />
-        <Route path="/student-details" element={<StudentDetails/>} />
-
+        <Route path="/admission-form" element={<AdmissionForm />} />
+        <Route path="/student-details" element={<StudentDetails />} />
+        <Route
+          path="/students-list/:registrationNumber"
+          element={<StudentsList />}
+        />
       </Routes>
-      <Footer/>
+      {/* <Footer/> */}
     </BrowserRouter>
   );
 };
