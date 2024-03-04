c032e9ba2713c2ae87c7dd29968edb2992b7b3f0
manish7479dlp <manish7479dlp@gmail.com>
2024-03-04 23:17:53 +0530

    fix: typo

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AdmissionForm from "../../pages/student/AdmissionForm";
 import StudentDetails from "../../pages/student/StudentDetails";
 import StudentsList from "../../components/StudentsList";
@@ -8,21 +8,23 @@ import SideBar from "../../components/Sidebar";
 
 const Dashboard = () => {
   return (
-    <div className="flex ">
+    <div className="flex">
       <SideBar />
-      <Content />
+      
+        <Content />
     </div>
   );
 };
 
 const Content = () => {
   return (
-    <div className="border flex-1 overflow-auto h-screen">
+    <div className="border overflow-auto h-screen w-full">
       <Routes>
+        <Route path="/" element={<DashboardContent/>} />
         <Route path="/admission-form" element={<AdmissionForm />} />
         <Route path="/student-details" element={<StudentDetails />} />
         <Route
-          path="/students-list/:registrationNumber"
+          path="/students-list"
           element={<StudentsList />}
         />
 
@@ -31,5 +33,13 @@ const Content = () => {
     </div>
   );
 };
+
+const DashboardContent = () => {
+  return (
+       <div className="bg-red-700">
+               Dashboard
+       </div>
+    )
+}
 
 export default Dashboard;
