6feb96f521acc09b6db4ffca7d8f0b350ec1f962
manish7479dlp <manish7479dlp@gmail.com>
2024-03-04 20:47:04 +0530

    refact: app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import AdmissionForm from "./pages/student/AdmissionForm";
-import StudentDetails from "./pages/student/StudentDetails";
-import StudentsList from "./components/StudentsList";
-import AllStudentsList from "./pages/student/AllStudentsList";
 import Dashboard from "./pages/dashboard/Dashboard";
 
 const App = () => {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admission-form" element={<AdmissionForm />} />
-        <Route path="/student-details" element={<StudentDetails />} />
-        <Route
-          path="/students-list/:registrationNumber"
-          element={<StudentsList />}
-        />
-
-        <Route path="/all-students-details" element={<AllStudentsList />} />
-        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/dashboard/*" element={<Dashboard />} />
       </Routes>
-      {/* <Footer/> */}
     </BrowserRouter>
   );
 };
