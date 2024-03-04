d1f43bec7e5c057b4cf8e86ed60d5c0056d6c738
manish7479dlp <manish7479dlp@gmail.com>
2024-03-05 00:58:28 +0530

    feat: add student related feature in the dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -5,28 +5,33 @@ import StudentDetails from "../../pages/student/StudentDetails";
 import StudentsList from "../../components/StudentsList";
 import AllStudentsList from "../../pages/student/AllStudentsList";
 import SideBar from "../../components/Sidebar";
+import DateAndTime from "../../components/DateAndTime";
+import { IoMdLogOut } from "react-icons/io";
 
 const Dashboard = () => {
   return (
-    <div className="flex">
+    <div className="flex ">
       <SideBar />
-      
-        <Content />
+      <Content />
     </div>
   );
 };
 
 const Content = () => {
+  const logout = () => {
+    alert("this feature is not implemented yet.")
+  }
   return (
-    <div className="border overflow-auto h-screen w-full">
+    <div className="flex-1 h-screen overflow-auto ">
+      <div className="py-1 bg-slate-100 flex justify-between px-10 items-center">
+        <DateAndTime/>
+        <IoMdLogOut className="text-3xl bg-red-600 rounded-full text-white cursor-pointer p-1 hover:scale-110 duration-500" onClick={logout}/>
+      </div>
       <Routes>
-        <Route path="/" element={<DashboardContent/>} />
+        <Route path="/" element={<DashboardContent />} />
         <Route path="/admission-form" element={<AdmissionForm />} />
         <Route path="/student-details" element={<StudentDetails />} />
-        <Route
-          path="/students-list"
-          element={<StudentsList />}
-        />
+        <Route path="/students-list" element={<StudentsList />} />
 
         <Route path="/all-students-details" element={<AllStudentsList />} />
       </Routes>
@@ -35,11 +40,7 @@ const Content = () => {
 };
 
 const DashboardContent = () => {
-  return (
-       <div className="bg-red-700">
-               Dashboard
-       </div>
-    )
-}
+  return <div className="bg-red-700 flex-1">Dashboard</div>;
+};
 
 export default Dashboard;
