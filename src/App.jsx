639c5632deec565e6114ef8073d4fe2e2d52e1cd
manish7479dlp <manish7479dlp@gmail.com>
2024-05-21 15:04:41 +0530

    add: react toastify conainer to the app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        draggable
+        theme="colored"
+      />
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/login" element={<Login />} />
