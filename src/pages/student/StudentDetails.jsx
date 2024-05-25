b3c5ab6b700de205c9012f43d8b85d8b9e92d2cb
manish7479dlp <manish7479dlp@gmail.com>
2024-05-25 19:28:35 +0530

    ui: add edit student details functionality

--- src/pages/student/StudentDetails.jsx
+++ src/pages/student/StudentDetails.jsx
@@ -44,8 +44,9 @@ const StudentDetails = () => {
         {NAVIGATION_DATA[0] === navigationTab && (
           <PersonalDetails
             name={studentDetails?.name}
-            // avatar={"https://avatars.githubusercontent.com/u/89635138?v=4"}
+            avatar={studentDetails?.avatar}
             studentDetails={Object.entries(studentDetails)}
+            details={studentDetails}
           />
         )}
 
@@ -443,10 +444,14 @@ const Documents = () => {
   );
 };
 
-const PersonalDetails = ({ studentDetails, name, avatar }) => {
-  const changeStudentDetails = () => {
-    toast.warning("This feature is not implemented yet");
-  };
+const PersonalDetails = ({ studentDetails, name, avatar, details }) => {
+  const [editStudentDetails, setEditStudentDetails] = useState(false);
+  const changeStudentDetails = () => {};
+
+  if (editStudentDetails) {
+    return <EditStudentDetails studentDetails={details} />;
+  }
+
   return (
     <div>
       <div className=" shadow-md p-4 flex flex-row justify-between pl-5 bg-slate-50">
@@ -465,7 +470,7 @@ const PersonalDetails = ({ studentDetails, name, avatar }) => {
 
         <div className="flex items-center">
           <button
-            onClick={changeStudentDetails}
+            onClick={() => setEditStudentDetails(true)}
             className="inline px-5 py-2 rounded-lg bg-yellow-500 text-white font-nunito font-bold cursor-pointer hover:scale-110 hover:bg-green-500 duration-700"
           >
             Edit Data
@@ -524,6 +529,163 @@ const StudentDataField = ({ label, value }) => {
         {labelName} :
       </label>
       <p className="text-md  text-slate-500">{value}</p>
+    </div>
+  );
+};
+
+const EditStudentDetails = ({ studentDetails }) => {
+  const [name, setName] = useState(studentDetails?.name);
+  const [fathersName, setFathersName] = useState(studentDetails?.fathersName);
+  const [mothersName, setMothersName] = useState(studentDetails?.mothersName);
+  const [guardianName, setGuardianName] = useState(
+    studentDetails?.guardianName
+  );
+  const [guardianMobileNumber, setGuardianMobileNumber] = useState(
+    studentDetails?.guardianMobileNumber
+  );
+  const [mobileNumber, setMobileNumber] = useState(
+    studentDetails?.mobileNumber
+  );
+  const [address, setAddress] = useState(studentDetails?.address);
+  const [state, setState] = useState(studentDetails?.state);
+  const [pincode, setPincode] = useState(studentDetails?.pinCode);
+  const [tenthPercentage, settenthPercentage] = useState(
+    studentDetails?.tenthPercentage
+  );
+  const [twelvePercentage, settwelvePercentage] = useState(
+    studentDetails?.twelfthPercentage
+  );
+  const [diplomaCGPA, setDiplomaCGPA] = useState(studentDetails?.diplomaCGPA);
+  const [image, setImage] = useState("");
+
+  //handle submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Clicked sucessfully");
+  };
+  return (
+    <div className="px-10 py-5">
+      <div className="m-2 mb-4">
+        <h1 className="text-4xl font-nunito font-extrabold text-slate-500">
+          Edit Student Details
+        </h1>
+        <div className="w-52 bg-cyan-500 h-[1.5px] mt-2 rounded-lg text-center"></div>
+      </div>
+      <form onSubmit={handleSubmit} className="grid gap-5">
+        <InputField
+          labelName={"Name"}
+          fieldValue={name}
+          onChangeHandler={setName}
+        />
+        <InputField
+          labelName={"Father's Name"}
+          fieldValue={fathersName}
+          onChangeHandler={setFathersName}
+        />
+        <InputField
+          labelName={"Mother's Name"}
+          fieldValue={mothersName}
+          onChangeHandler={setMothersName}
+        />
+        <InputField
+          labelName={"Guardian Name"}
+          fieldValue={guardianName}
+          onChangeHandler={setGuardianName}
+        />
+        <InputField
+          labelName={"Guardian Mobile Number"}
+          inputType="number"
+          fieldValue={guardianMobileNumber}
+          onChangeHandler={setGuardianMobileNumber}
+        />
+        <InputField
+          labelName={"Mobile Number"}
+          inputType="number"
+          fieldValue={mobileNumber}
+          onChangeHandler={setMobileNumber}
+        />
+        <InputField
+          labelName={"Address"}
+          fieldValue={address}
+          onChangeHandler={setAddress}
+        />
+        <InputField
+          labelName={"State"}
+          fieldValue={state}
+          onChangeHandler={setState}
+        />
+        <InputField
+          labelName={"Pincode"}
+          inputType="number"
+          fieldValue={pincode}
+          onChangeHandler={setPincode}
+        />
+        <InputField
+          labelName={"10th%"}
+          fieldValue={tenthPercentage}
+          inputType="number"
+        />
+        <InputField
+          labelName={"12th%"}
+          require={false}
+          fieldValue={twelvePercentage}
+          onChangeHandler={settwelvePercentage}
+        />
+        <InputField
+          labelName={"Diploma CGPA"}
+          require={false}
+          inputType="number"
+          fieldValue={diplomaCGPA}
+          onChangeHandler={setDiplomaCGPA}
+        />
+        <FileInputField labelName={"Image"} onChangeHandler={setImage} />
+
+        <div className="col-span-3 text-center ">
+          <input
+            type="submit"
+            value={"Apply"}
+            className="px-5 py-2 rounded-lg bg-yellow-500 text-white  font-nunito font-bold cursor-pointer hover:scale-110 hover:bg-green-500 duration-700"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const InputField = ({
+  labelName,
+  fieldValue,
+  onChangeHandler,
+  require = true,
+  inputType,
+}) => {
+  return (
+    <div>
+      <label className="font-semi-bold">{labelName}</label>
+      {require && <span className="text-red-600 font-bold ">*</span>}
+      <br />
+      <input
+        type={inputType}
+        value={fieldValue}
+        onChange={(e) => onChangeHandler(e.target.value)}
+        className="rounded-md px-2 py-2 text-xl font-semibold mt-2 border border-slate-700 w-80"
+      />
+    </div>
+  );
+};
+
+const FileInputField = ({ labelName, onChangeHandler }) => {
+  return (
+    <div>
+      <label className="font-semi-bold">{labelName}</label>
+      <span className="text-red-600 font-bold">*</span>
+      <br />
+      <input
+        type="file"
+        onChange={(e) => onChangeHandler(e.target.files[0])}
+        className="rounded-md px-2 py-2 mt-2 border border-slate-700 w-80"
+        placeholder="none"
+      />
     </div>
   );
 };
